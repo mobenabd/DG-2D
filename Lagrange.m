a52eadd1e1775c2f84f475e868975198fe55003e
mbenabdelou <m.benabde21@gmail.com>
2023-02-20 17:05:17 +0100

    add .gitignore && Lagrange second deriviatives

--- Lagrange.m
+++ Lagrange.m
@@ -35,5 +35,31 @@ if derv==1
     end 
 end
 
+if derv==2
+    val = 0;
+    for j=1:l %somme
+        if (j ~= i)
+            tmp = 1/(nodes(i) - nodes(j));
+            
+            tmp2 = 0;
+            for m=1:l  %%somme
+                if (m ~= i && m ~= j)
+                    tmp1 = 1/(nodes(i) - nodes(m)); 
+                     
+                    for nn=1:l  %%produit
+                        if (nn ~= i && nn ~= j && nn ~= m)
+                             tmp1 = tmp1.*(x-nodes(nn))/(nodes(i) - nodes(nn));
+                        end
+                    end
+                   tmp2 = tmp2 + tmp1;
+                end 
+            end
+            tmp = tmp*tmp2;
+            
+            val = val + tmp;
+        end
+        
+    end
+end
 
 end
